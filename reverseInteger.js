e67281b9909208e640a97af626b151d29ce93031
kwakster4 <kimbyung4@utexas.edu>
2021-06-11 16:27:51 -0500

    implemented reverseInteger

---
+++ reverseInteger.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/reverse-integer/
+
+// Input: number
+// Output: number with reversed Integers
+// Constraints: x is always between -2^31 and 2^31 - 1
+
+var reverse = function(x) {
+  let neg = false;
+  if (x < 0) { neg = true; }
+  let numStr = x.toString();
+  let result = '';
+  if (neg) {
+    numStr = numStr.slice(1);
+    result = '-';
+  }
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    result += numStr[i];
+  }
+  if (parseInt(result) > (2 ** 31 - 1) || parseInt(result) < (-2 ** 31)) {
+    return 0;
+  }
+  return parseInt(result);
+};
