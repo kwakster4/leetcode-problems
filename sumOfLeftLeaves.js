4d30014a683fee02eab3d112e0c8259d8b7725d4
kwakster4 <kimbyung4@utexas.edu>
2021-06-14 15:27:13 -0500

    implemented sumOfLeftLeaves with assumption that root is passed in as the relevant data structure

---
+++ sumOfLeftLeaves.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/sum-of-left-leaves/
+// Given root of a binary tree, return the sum of all left leaves
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Input: param {TreeNode} root
+ * Output: return {number}
+ */
+ var sumOfLeftLeaves = function(root) {
+  // initialize sum variable
+  // make recursion function that goes through all the children of the node, and adds the left leaf to the param
+  let sum = 0;
+  let leftTraverse = function(node) {
+    if (node.left) {
+      sum += node.left;
+    }
+    if (node.left) {leftTraverse(node.left);}
+    if (node.right) {leftTraverse(node.right);}
+  }
+  leftTraverse(root);
+  return sum;
+};
