6491c1fcc18b4c0672d16e2a82db9ca40749d155
kwakster4 <kimbyung4@utexas.edu>
2021-06-30 14:54:49 -0500

    wrote comment for algorithm method used to complete containsDuplicate.js

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -4,6 +4,7 @@
 // Output: true if any value appears at least twice in array, false if every element distinct
 // Constraints: 1 <= nums.length <= 10^5; integer will be +- 10^9;
 // Edge: num is 0, which is falsy
+// Hash Table Method
 var containsDuplicate = function(nums) {
   let bank = {};
   for (let num of nums) {
